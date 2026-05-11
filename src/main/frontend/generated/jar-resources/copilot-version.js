9ea5cfdbbc87dcf539f1bde2de2fe5f4feadf184
drewdrew0414 <drew0414drew@gmail.com>
2026-05-11 23:02:36 +0900

    default Files

--- src/main/frontend/generated/jar-resources/copilot-version.js
+++
@@ -1,1 +0,0 @@
-// Full cdn version: 25.1.4-undefined
--- src/main/frontend/generated/jar-resources/copilot.js
+++
@@ -1,5 +0,0 @@
-import { az as a, ay as i } from "./copilot/copilot-C7bhBd6M.js";
-export {
-  a as createChildrenDefinitions,
-  i as registerImporter
-};
